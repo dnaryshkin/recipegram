cb78d52fe3dfc572e6ef68b50bb28f50af40e651
dnaryshkin <dmitriynaryshkin@yandex.ru>
2025-04-27 22:11:18 +0300

    Добавлен сериализатор для модели Пользователя.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,6 +2,8 @@ from django.core.files.base import ContentFile
 from rest_framework import serializers
 import base64
 
+from backend.users.models import User, Subscription
+
 
 class Base64ImageField(serializers.ImageField):
     """Сериализатор для изображений в формате base64."""
@@ -12,3 +14,32 @@ class Base64ImageField(serializers.ImageField):
             ext = format.split('/')[-1]
             data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
         return super().to_internal_value(data)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Пользователя."""
+    is_subscribed = serializers.SerializerMethodField()
+    avatar = Base64ImageField()
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed',
+            'avatar',
+        )
+
+    def get_is_subscribed(self, obj):
+        """Функция проверки подписки пользователя на автора."""
+        user = self.context.get('request').user
+        if user.is_authenticated:
+            return Subscription.objects.filter(
+                user=user,
+                following=obj
+            ).exists()
+        return False
+
