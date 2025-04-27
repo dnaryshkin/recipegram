41d80ed8e85d76e4a46cb58ee1079da9398c3762
dnaryshkin <dmitriynaryshkin@yandex.ru>
2025-04-27 21:58:43 +0300

    Создано приложение Api Добавлен сериализатор Base64ImageField из kittygram

---
+++ backend/api/serializers.py
@@ -0,0 +1,14 @@
+from django.core.files.base import ContentFile
+from rest_framework import serializers
+import base64
+
+
+class Base64ImageField(serializers.ImageField):
+    """Сериализатор для изображений в формате base64."""
+    def to_internal_value(self, data):
+        """Функция декодирования данных base64."""
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+        return super().to_internal_value(data)
