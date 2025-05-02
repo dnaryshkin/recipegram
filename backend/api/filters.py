e2b4d2dfb93ee3d0133adcf0980f95a18c2f2bef
dnaryshkin <dmitriynaryshkin@yandex.ru>
2025-05-03 00:51:24 +0300

    Добавлен фильтр по наименованию для ингредиентов.

---
+++ backend/api/filters.py
@@ -0,0 +1,16 @@
+from django_filters import rest_framework as filters
+import django_filters
+
+from backend.recipes.models import Ingredient
+
+
+class IngredientFilter(filters.FilterSet):
+    """Класс для фильтрации ингредиентов по названию."""
+    name = django_filters.CharFilter(
+        field_name='name',
+    )
+
+
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
