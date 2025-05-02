9f7e028ea8cbe20cf15690b5e2943e831d394f54
dnaryshkin <dmitriynaryshkin@yandex.ru>
2025-05-02 18:47:00 +0300

    Добавлен сериализатор списка покупок.

---
+++ backend/api/serializers/shoppinglist.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from backend.api.serializers.recipes import MiniRecipeSerializer
+from backend.recipes.models import RecipesInShoppingList
+
+
+class ShoppingListSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели добавления в список покупок."""
+
+    class Meta:
+        model = RecipesInShoppingList
+        fields = ('user', 'recipe')
+
+    def validate(self, data):
+        """Функция проверки добавления в список покупок."""
+        user = self.context['request'].user
+        recipe = data['recipe']
+        if RecipesInShoppingList.objects.filter(
+                user=user,
+                recipe=recipe
+        ).exists():
+            raise serializers.ValidationError(
+                'Рецепт уже добавлен в список покупок!'
+            )
+        return data
+
+    def to_representation(self, instance):
+        return MiniRecipeSerializer(instance.recipe, context=self.context).data
