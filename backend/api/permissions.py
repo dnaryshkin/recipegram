90e93d8acc13af57bf37d43600e0958c85bbf381
dnaryshkin <dmitriynaryshkin@yandex.ru>
2025-05-03 21:07:30 +0300

    правки пермишена

---
+++ backend/api/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsAdminAuthorOrReadOnly(BasePermission):
+    """
+    GET-запросы доступны всем пользователям;
+    POST-запросы доступны только аутентифицированным пользователям;
+    PATCH и DELETE-запросы доступны автору, модератору или администратору.
+    """
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return (
+            obj.author == request.user
+            or request.user.is_admin
+        )
