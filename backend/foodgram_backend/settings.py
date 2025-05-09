7280ccdd667b3a796e76f90350eaabea94a4686e
dnaryshkin <dmitriynaryshkin@yandex.ru>
2025-05-09 16:40:33 +0300

    правка redoc ссылки тест 1

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'foodgram_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['foodgram'/'docs'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
