abdad2bcb5ea00e9b45d9b26df7e6a8ecda0bcf4
dnaryshkin <dmitriynaryshkin@yandex.ru>
2025-05-09 16:27:46 +0300

    правка redoc ссылки

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -8,7 +8,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('redoc', include('frontend.urls')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
