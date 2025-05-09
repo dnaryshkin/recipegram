6fb814a54bbe1e0455a7a1e59888c5f84a1beeab
dnaryshkin <dmitriynaryshkin@yandex.ru>
2025-05-10 02:31:55 +0300

    правки суммы

--- backend/api/shopping_list.py
+++ backend/api/shopping_list.py
@@ -1,6 +1,6 @@
 from django.db.models import Sum
 
-from recipes.models import IngredientInRecipe, Recipe
+from recipes.models import IngredientInRecipe
 
 
 class CreateShoppingList(object):
@@ -9,21 +9,19 @@ class CreateShoppingList(object):
     @staticmethod
     def create_list(user):
         """Функция создания списка покупок."""
-        recipes = Recipe.objects.filter(in_shopping_lists__user=user)
-        ingredients = (
-            IngredientInRecipe.objects.filter(recipe__in=recipes)
-            .values(
-                'ingredient__name',
-                'ingredient__measurement_unit',
-            )
-            .annotate(total_amount=Sum('amount'))
-            .order_by('ingredient__name')
+        ingredients = IngredientInRecipe.objects.filter(
+            recipe__in_shopping_lists__user=user
+        ).values(
+            'ingredient__name',
+            'ingredient__measurement_unit',
+        ).annotate(
+            total_amount=Sum('amount'),
         )
         shopping_list = []
         for ingredient in ingredients:
-            name = ingredient['ingredient__name']
-            unit = ingredient['ingredient__measurement_unit']
-            amount = ingredient['total_amount']
-            ingredient_shopping = f'{name} ({unit}) — {amount}'
-            shopping_list.append(ingredient_shopping)
-        return shopping_list
+            line = (
+                f"{ingredient['ingredient__name']} "
+                f"({ingredient['ingredient__measurement_unit']}) — "
+                f"{ingredient['total_amount']}"
+            )
+            shopping_list.append(line)
