1550a7fe55ee06832e2f77b7de24d519081f5aad
dnaryshkin <dmitriynaryshkin@yandex.ru>
2025-05-11 14:15:05 +0300

    в моделе избранное установлено related_name для админки

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -193,6 +193,7 @@ class Favorite(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
+        related_name='favorites',
     )
 
     def __str__(self):
